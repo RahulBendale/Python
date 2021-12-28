5dfb9262ce1747a0bee325dfef1242b6b50c7bf1
RahulBendale <40611128+RahulBendale@users.noreply.github.com>
2021-12-28 16:44:12 +0530

    Update Palindrom.py

--- Palindrom.py
+++ Palindrom.py
@@ -1,8 +1,1 @@
-str1 = input("Enter the string:")
-str1 = str1.casefold()
-rev_str = reversed(str1)
-
-if list(str1) == list(rev_str):
-   print("The string is a palindrome.")
-else:
-   print("The string is not a palindrome.")
+print("Hello Accenture")
